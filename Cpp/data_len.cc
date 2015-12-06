33b5b511a720d3b14376b96a727705551d49db27
Sun Mai <sun.mai@outlook.com>
2015-12-06 20:28:32 +0100

    add cpp scripts

---
+++ Cpp/data_len.cc
@@ -0,0 +1,23 @@
+/*
+ * FILE
+ * Copyright © constra
+ * Email: sun.mai@outlook.com
+ */
+
+#include <iostream>
+using namespace std;
+
+int
+main( int argc, char *argv[] )
+{
+    cout << "Memory size of the data types in C++" << endl;
+    cout << "Size of char : " << sizeof(char) << "byte" << endl;
+    cout << "Size of int : " << sizeof(int) << "byte" << endl;
+    cout << "Size of short int : " << sizeof(short int) << "byte" << endl;
+    cout << "Size of long int : " << sizeof(long int) << "byte" << endl;
+    cout << "Size of float : " << sizeof(float) << "byte" << endl;
+    cout << "Size of double : " << sizeof(double) << "byte" << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << "byte" << endl;
+
+    return 0;
+}
---
+++ Cpp/world2.cc
@@ -0,0 +1,15 @@
+/*
+ * FILE
+ * Copyright © constra
+ * Email: sun.mai@outlook.com
+ */
+
+#include <iostream>
+using namespace std;
+
+int
+main( int argc, char *argv[] )
+{
+    cout << "work!" << endl;
+	return 0;
+}
