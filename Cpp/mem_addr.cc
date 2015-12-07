e461049a11efee5b9c4bfdff0dc718bc412d2e78
Sun Mai <sun.mai@outlook.com>
2015-12-07 19:32:36 +0100

    print memory address

---
+++ Cpp/mem_addr.cc
@@ -0,0 +1,16 @@
+/*
+ * FILE
+ * Copyright © constra
+ * Email: sun.mai@outlook.com
+ */
+
+#include <iostream>
+using namespace std;
+
+int
+main( int argc, char *argv[] )
+{
+    int num = 5;
+    cout << "the memory address is: " << &num << endl;
+	return 0;
+}
